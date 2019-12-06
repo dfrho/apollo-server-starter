f249a136a51a5cf95a8b4a5f8dfffecfc7693d53
David Rhodes <david@davidrhodes.work>
2019-12-05 19:03:14 -0600

    feat(validation): adds validation and error handling

--- src/models/message.js
+++ src/models/message.js
@@ -2,6 +2,13 @@ const message = (sequelize, DataTypes) => {
   const Message = sequelize.define('message', {
     text: {
       type: DataTypes.STRING,
+      validate: {
+        notEmpty:
+        {
+          args: true,
+          msg: 'a message requires text'
+        },
+      },
     },
   });
 
